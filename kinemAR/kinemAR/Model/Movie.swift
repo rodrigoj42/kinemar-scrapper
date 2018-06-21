37f0cc158935c0020f6bddd7588915d5968100f8
Rafael Damasceno <rdamasceno@poli.ufrj.br>
2018-06-21 02:03:42 -0300

    Add Movie model +  Adds option to movie history + Organize files

---
+++ kinemAR/kinemAR/Model/Movie.swift
@@ -0,0 +1,11 @@
+class Movie {
+    var name: String!
+    var originalName: String?
+    var description: String?
+    var productionCompany: String?
+    var countryOfOrigin: String?
+    var director: String?
+    var actors: String?
+    var ticket: String?
+    var trailer: String?
+}
