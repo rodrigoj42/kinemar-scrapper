e4815446eda0dc07d432958eb92acac2cf25e1d3
Rafael Damasceno <rdamasceno@poli.ufrj.br>
2018-06-29 22:59:01 -0300

    Update Movie model

--- kinemAR/kinemAR/Model/Movie.swift
+++ kinemAR/kinemAR/Model/Movie.swift
@@ -1,11 +1,31 @@
+enum RatingSource: String {
+    case imdb = "IMDb"
+    case rottenTomatoes = "Rotten Tomatoes"
+    case metacritic = "Metacritic"
+}
+
+class Ratting {
+    var source: RatingSource!
+    var value: String!
+}
+
 class Movie {
-    var name: String!
-    var originalName: String?
-    var description: String?
-    var productionCompany: String?
-    var countryOfOrigin: String?
-    var director: String?
-    var actors: String?
-    var ticket: String?
-    var trailer: String?
+    var title: String!
+    var originalTitle: String!
+    var description: String!
+    var director: String!
+    var ticket: String!
+    var production: String!
+    var countryOfOrigin: String!
+    var contentRating: String!
+    var ratings: [Ratting]!
+    var writer: String!
+    var actors: String!
+    var website: String!
+    var genre: String!
+    var language: String!
+    var runtime: String!
+    var imdbID: String!
+    var year: String!
+    var trailer: String!
 }
